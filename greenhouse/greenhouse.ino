147f940054ba8176d6a0bf944d2e1d112578061c
Chris Pitzer <chrispitzer@gmail.com>
2017-08-13 12:00:51 -0700

    better event listening

--- greenhouse/greenhouse.ino
+++ greenhouse/greenhouse.ino
@@ -6,13 +6,27 @@
 // custom stuff.
 byte smiley[8] = {
   B00000,
-  B10001,
   B00000,
+  B01010,
   B00000,
   B10001,
   B01110,
   B00000,
 };
+
+int readkey;
+
+// Button impediance values
+#define rightButton 0
+#define upButton 131
+#define downButton 305
+#define leftButton 478
+#define selectButton 720
+#define noButton 1023
+
+int buttonPressedThisLoop;
+int buttonPressedLastLoop;
+
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -46,11 +60,57 @@ void setup() {
 }
 
 void loop() {
+  buttonPressedThisLoop = readButtons();
+  if(buttonPressedThisLoop == buttonPressedLastLoop) {
+    return;
+  }
+  buttonPressedLastLoop = buttonPressedThisLoop;
 
+  lcd.clear();
 
+  switch (buttonPressedThisLoop) {
+    case leftButton:
+      lcd.print("left");
+      break;
+    case rightButton:
+      lcd.print("right");
+      break;
+    case upButton:
+      lcd.print("up");
+      break;
+    case downButton:
+      lcd.print("down");
+      break;
+    case selectButton:
+      lcd.print("select");
+      break;
+    default:
+      lcd.print("nothing");
+      break;
+  }
 }
 
+int readButtons() {
+  readkey = analogRead(0);
 
+  if (readkey < rightButton + 75) {
+    return rightButton;
+  }
+  if (readkey < upButton + 75) {
+    return upButton;
+  }
+  if (readkey < downButton + 75) {
+    return downButton;
+  }
+  if (readkey < leftButton + 75) {
+    return leftButton;
+  }
+  if (readkey < selectButton + 75) {
+    return selectButton;
+  }
+
+  return noButton;
+}
 /*
  *  chamber 1
  *    humidity
@@ -94,7 +154,7 @@ CUSTOM CHARACTERS
   you can only have 8 custom characters, with indexes 0-7
   byte smiley[8] = {
     B00000,
-    B10001,
+    B01010,
     B00000,
     B00000,
     B10001,
